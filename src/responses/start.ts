1320460f3ca0ddcfbd39eec97dc9346e0e8a12d2
DigitalLearningCompany <162745763+DigitalLearningCompany@users.noreply.github.com>
2024-04-10 11:56:07 -0300

    Update start.ts

--- src/responses/start.ts
+++ src/responses/start.ts
@@ -17,10 +17,6 @@ const introMessage = (name: string) => fmt`
   "Depósito feito por mim na sua conta da corretora"
 )} (Não é sorteio, é garantido que vou fazer, é só seguir o passo a passo e pronto!)
 
-🎁 ${bold(
-  "Depósito feito por mim na sua conta da corretora"
-)} (Não é sorteio, é garantido que vou fazer, é só entrar no grupo agora!) 
-
 ⬇️ Últimas vagas/horas para você receber tudo isso. Clique no botão abaixo, entre no grupo, e siga o passo a passo AGORA!
 `;
 
